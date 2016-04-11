db015f8829db8fd189c152363d6552c0729a4c2d
Bruno Brant <brubrant@gmail.com>
2016-04-11 14:37:10 -0300

    Pequenas melhorias

--- theme/functions.php
+++ theme/functions.php
@@ -27,8 +27,9 @@ if (function_exists('add_theme_support'))
 
     // Add Thumbnail Theme Support
     add_theme_support('post-thumbnails');
-    add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
+    // add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
 
+    
     // Add Support for Custom Backgrounds - Uncomment below if you're going to use
     /*add_theme_support('custom-background', array(
 	'default-color' => 'FFF',
